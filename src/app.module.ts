5c600bf68a4b5958e6c0bee95ae092e73332af0b
Yosiak-alv <Josias_romero60@outlook.com>
2024-07-14 02:06:11 -0600

    installing necessary packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {GraphQLModule} from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { LessonModule } from './lesson/lesson.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost:27017',
+      database: 'school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true, // set to true to generate schema file automatically when the app starts
+      driver: ApolloDriver,
+      playground: true,
+    }),
+    LessonModule,
+    StudentModule
+  ],
+  providers: [],
 })
 export class AppModule {}
